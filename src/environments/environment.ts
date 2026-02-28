461bb4f6c788fc05977e7dff4e299b197c5feeb5
Aristedes Novoa <arisoft1@gmial.com>
2026-02-28 11:52:48 -0500

    Prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  API_BASE: 'https://curs-000537-ang21advanced-deployment.onrender.com'
+  //API_BASE: 'https://curs-000537-ang21advanced-deployment.onrender.com'
+  API_BASE: 'https://curs-000537-ang21advanced-deployment-reif.onrender.com'
 };
